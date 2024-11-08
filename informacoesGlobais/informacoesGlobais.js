87c6bd403327e602d1314b89b12bc52851419364
Laur4xl <107406784+Laur4xl@users.noreply.github.com>
2024-11-08 11:48:37 +0000

    vfvhj

--- informacoesGlobais/informacoesGlobais.js
+++ informacoesGlobais/informacoesGlobais.js
@@ -3,7 +3,7 @@ const url= 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-gl
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    const pessoasConectadas = (dados.totais_pessoas_conectadas / 1e9)
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     const horas = parseInt(dados.tempo_medio)
     const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo)*100).toFixed(2);
